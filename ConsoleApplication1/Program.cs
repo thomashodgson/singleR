29647c98f5d6085528118ec42552f4ed3c969c21
Thomas.hodgson <thomas.hodgson@red-gate.com>
2015-10-02 14:35:09 +0100

    Everything in same unit, I know it works I have manually tested it for a few cases. Usually if I were to write the whole app in a single unit it would take a long time to run each test case. I can change things quickly, the DRY principle i.e. the method IsMine means there is no duplication and all changes would just be made in one place. However if i were to change something I could not be sure I hadnt broken the other things. I would also be breaking the open closed principle, i.e. what ever change i make i would be touching 100% of the source code. All this leads me to believe I need to test this class so i can be sure it works and will work in the future

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var chars = input.ToCharArray();
+
+            var result = new char[chars.Length];
+
+            for (var i = 0; i < chars.Length; ++i)
+            {
+                if (IsMine(chars[i]))
+                {
+                    result[i] = '*';
+                    continue;
+                }
+
+                int count = 0;
+                if (i > 0 && IsMine(chars[i - 1]))
+                {
+                    count ++;
+                }
+                if (i < chars.Length - 1 && IsMine(chars[i + 1]))
+                {
+                    count ++;
+                }
+
+                result[i] =  count.ToString()[0];
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static bool IsMine(char character)
+        {
+            return character == '*';
+        }
+    }
+}
